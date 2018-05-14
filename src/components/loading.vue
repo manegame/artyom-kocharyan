09355c1725b0e4ff445dc8a405c6626be5102d46
manegame <manusnijhoff@gmail.com>
2018-05-14 12:56:11 +0200

    do something with image on home page

---
+++ src/components/loading.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class='loading main__cluster main__cluster--1'>
+    Loading
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loading'
+}
+</script>
+
+<style lang='scss'>
+
+.loading {
+  &::after {
+    overflow: hidden;
+    display: inline-block;
+    vertical-align: bottom;
+    -webkit-animation: ellipsis steps(4, end) 900ms infinite;
+    animation: ellipsis steps(4, end) 900ms infinite;
+    content: "\2026"; /* ascii code for the ellipsis character */
+    width: 0;
+  }
+}
+
+@keyframes ellipsis {
+  to {
+    width: 1.25em;
+  }
+}
+
+@-webkit-keyframes ellipsis {
+  to {
+    width: 1.25em;
+  }
+}
+</style>
--- src/views/main.vue
+++ src/views/main.vue
@@ -4,23 +4,30 @@
     <router-link v-if='main.posts.length'
           v-for='(post, index) in main.posts'
           tag='div'
-          class="main__cluster"
+          class="main__cluster main__link"
           :to='{name: "single", params: {slug: post.slug}}'
           :key='post.id'
           :class='"main__cluster--" + (post.index)'>
-          <a v-html='post.title.rendered' @click.prevent/>
+          <a  class='main__link__title'
+              @click.prevent 
+              :style='{ backgroundImage: "url(" + post.acf.images[0].image.sizes["medium"] + ")" }'>
+              <span v-html='post.title.rendered' />
+          </a>
     </router-link>
+    <loading v-else/>
   </div>
 </template>
 
 <script>
 import {mapState} from 'vuex'
 import headbar from '../components/headbar'
+import loading from '../components/loading'
 
 export default {
   name: 'main',
   components: {
-    headbar
+    headbar,
+    loading
   },
   computed: {
     ...mapState(['main'])
@@ -35,4 +42,19 @@ export default {
 @import '../style/helpers/_reset.css';
 @import '../style/_variables.scss';
 @import '../style/_main.scss';
+
+.main__link__title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: 1000px 1000px;
+
+  &:hover {
+    background-position: center center;
+  }
+}
 </style>
